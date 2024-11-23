55eac011c97c760155e3a13589f6e971ccb6b5ac
Alexander Hartmann <ahartman30@users.noreply.github.com>
2024-11-23 15:28:53 +0100

    Fixed setting meta tag.

--- mod_weatherrefresh/mod_weatherrefresh.php
+++ mod_weatherrefresh/mod_weatherrefresh.php
@@ -8,5 +8,5 @@ $refreshTime = $params->get('refreshTime');
 if (is_numeric($refreshTime) && $refreshTime > 0) {
   $refreshTime *= 60;
   $document = Factory::getApplication()->getDocument();
-  $document->setMetaData("refresh", $refreshTime);
+	$document->setMetaData("refresh", $refreshTime, "http-equiv");
 }
